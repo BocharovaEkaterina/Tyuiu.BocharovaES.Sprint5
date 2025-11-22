41bfd923f7e7d92eb186dac992b5ea86aaef8d3d
Екатерина Бочарова <kati.bocharova.06@mail.ru>
2025-11-22 21:41:24 +0500

    Add project files.

---
+++ Tyuiu.BocharovaES.Sprint5.Task0.V27.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using Tyuiu.BocharovaES.Sprint5.Task0.V27.Lib;
+using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
+namespace Tyuiu.BocharovaES.Sprint5.Task0.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\Users\Пользователь\AppData\Local\Temp\OutPutFileTask0.txt";
+            FileInfo fileInfo= new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
