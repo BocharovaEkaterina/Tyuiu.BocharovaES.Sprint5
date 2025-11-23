af25f2019d9b5a0e6250a356bc4dd79fcd951280
Екатерина Бочарова <kati.bocharova.06@mail.ru>
2025-11-23 22:21:29 +0500

    Task2

---
+++ Tyuiu.BocharovaES.Sprint5.Task2.V10.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BocharovaES.Sprint5.Task2.V10.Lib;
+using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
+namespace Tyuiu.BocharovaES.Sprint5.Task2.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\Users\Пользователь\AppData\Local\Temp\OutPutFileTask2.csv";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
