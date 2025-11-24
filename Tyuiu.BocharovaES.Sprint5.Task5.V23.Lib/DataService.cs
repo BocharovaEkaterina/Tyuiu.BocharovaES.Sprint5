46345bfaf3cf741d58d045ef679957b1610fc121
Екатерина Бочарова <kati.bocharova.06@mail.ru>
2025-11-24 23:26:18 +0500

    Task5

---
+++ Tyuiu.BocharovaES.Sprint5.Task5.V23.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.BocharovaES.Sprint5.Task5.V23.Lib
+{
+    public class DataService : ISprint5Task5V23
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double res = 1000;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    line = line.Replace(".", ",");
+                    double[] a = line.Split(" ").Select(double.Parse).ToArray();
+                    for (int i = 0; i < a.Length; i++)
+                    {
+                        if (a[i] != Math.Truncate(a[i]))  
+                        {
+                            if (a[i] < res)  
+                            {
+                                res = a[i]; 
+                            }
+                        }
+                    }
+                }
+            }
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.BocharovaES.Sprint5.Task5.V23/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.BocharovaES.Sprint5.Task5.V23.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #5 | Выполнила: Бочарова Е. С. | ИИПб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Чтение набора данных из текстового файла                          *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #23                                                             *");
+        Console.WriteLine("* Выполнила: Бочарова Екатерина Сергеевна | ИИПб-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ :                                                               *");
+        Console.WriteLine("* Дан файл С:|DataSprint5|InPutDataFileTask5V23.txt, в котором есть       *");
+        Console.WriteLine("* набор значений. Найти минимальное вещественное число в файле. Полученный*");
+        Console.WriteLine("* результат вывести на консоль. У вещественных значений округлить до трёх *");
+        Console.WriteLine("* знаков после запятой.                                                   *");
+        Console.WriteLine("*                                                                         *");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ :                                                       *");
+        Console.WriteLine("***************************************************************************");
+
+        string path = @"C:\DataSprint5\InPutDataFileTask5V23.txt";
+
+        Console.WriteLine("Данные находятся в файле " + path);
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        double res = ds.LoadFromDataFile(path);
+
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
