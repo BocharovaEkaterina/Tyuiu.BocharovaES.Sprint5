84f0accf52a12e12baf9afd1c1e68383b838a714
Екатерина Бочарова <kati.bocharova.06@mail.ru>
2025-11-25 19:18:30 +0500

    Task6

---
+++ Tyuiu.BocharovaES.Sprint5.Task6.V23.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using Tyuiu.BocharovaES.Sprint5.Task6.V23.Lib;
+namespace Tyuiu.BocharovaES.Sprint5.Task6.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask6V23.txt";
+
+            var res = ds.LoadFromDataFile(path);
+            int wait =7;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BocharovaES.Sprint5.Task6.V23/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.BocharovaES.Sprint5.Task6.V23.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #5 | Выполнила: Бочарова Е. С. | ИИПб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Обработка текстовых файлов                                        *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #23                                                             *");
+        Console.WriteLine("* Выполнила: Бочарова Екатерина Сергеевна | ИИПб-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ :                                                               *");
+        Console.WriteLine("* Дан файл С:|DataSprint5|InPutDataFileTask6V23.txt, в котором есть       *");
+        Console.WriteLine("* набор символьных данных.Найти количество знаков тире в заданной строке. *");
+        Console.WriteLine("*                                                                         *");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ :                                                       *");
+        Console.WriteLine("***************************************************************************");
+
+        string path = @"C:\DataSprint5\InPutDataFileTask6V23.txt";
+
+        Console.WriteLine("Данные находятся в файле " + path);
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        double res = ds.LoadFromDataFile(path);
+
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
